0b7f867b3c68f8aa452da436b574c4c41d8d0e97
bohdanml <146237023+bohdanml@users.noreply.github.com>
2023-11-29 20:04:19 +0200

    Update UnitTest1.cpp

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -6,16 +6,14 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
 {
-	TEST_CLASS(UnitTest1)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-			char row[] = "neoniontonsonyo";
-			int k = 5;
-			int t = Count(row,1);
-			Assert::AreEqual(t, k);
-		}
-	};
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            char* str = "n2o4no";
+            Assert::AreEqual(Count(str), 1);
+        }
+    };
 }
